36ee4a4928da80192686fc3fd12b4606cb0a337f
varun115 <50090823+varun115@users.noreply.github.com>
2020-09-03 19:43:29 +0530

    Updates

---
+++ precache-manifest.c8da2f04883b1ae299667daa78406cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37f5d3ec63709cfddb89e271c41bac6",
+    "url": "/react_todo_app/index.html"
+  },
+  {
+    "revision": "f8843e0a9fef9b480db5",
+    "url": "/react_todo_app/static/css/main.8c26a148.chunk.css"
+  },
+  {
+    "revision": "fb4a452194d2fb686e3f",
+    "url": "/react_todo_app/static/js/2.a5f30b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_app/static/js/2.a5f30b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8843e0a9fef9b480db5",
+    "url": "/react_todo_app/static/js/main.70934156.chunk.js"
+  },
+  {
+    "revision": "4f029a2f56ddcd40086f",
+    "url": "/react_todo_app/static/js/runtime-main.ffd848d5.js"
+  }
+]);
